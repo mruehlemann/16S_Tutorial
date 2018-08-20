854bb9b753e349b21655fbdccaa4bd97db602a93
Malte Rühlemann <malte.ruehlemann@gmail.com>
2018-08-20 12:04:01 +0200

    Update get_data.sh

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,3 +1,5 @@
+print 'Please wait while the files are being downloaded. This will take a few minutes'
+
 cd raw_data
 for file in $(cut -f 12 | grep 'fastq/B' | tr ';' '\n'); do wget $file; done
 cd ..
