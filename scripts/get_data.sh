ddee23ca9421a79df22ffcd4cc8bdd022a888a93
Malte Rühlemann <malte.ruehlemann@gmail.com>
2019-09-20 10:28:20 +0200

    Update get_data.sh

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,5 +1,12 @@
 print 'Please wait while the files are being downloaded. This will take a few minutes'
 
 cd raw_data
-for file in $(cut -f 12 | grep 'fastq/B' | tr ';' '\n'); do wget $file; done
+for pair in $(cut -f 10,12 PRJEB12201.txt | tr '\t' ';' | grep 'fastq/B' ); do
+files=($(echo $pair | tr ';' ' '))
+f3name=$(echo ${files[2]} | awk -F'/' '{print $NF}')
+f4name=$(echo ${files[3]} | awk -F'/' '{print $NF}')
+wget -O $f3name ${files[0]}
+wget -O $f4name ${files[1]}
+done
+
 cd ..
